58b1001b38dedc6d632a8bee6b7a8369a68f6495
Bbatres@ <135394133+BATRUCHO@users.noreply.github.com>
2026-02-25 19:14:15 -0600

    examen

---
+++ Java/Proyectos/CasoEstudio/src/main/java/com/mycompany/casoestudio/CasoEstudio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.casoestudio;
+
+/**
+ *
+ * @author braya
+ */
+public class CasoEstudio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
