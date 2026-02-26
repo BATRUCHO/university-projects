208b20edb97a6f3de788c7c76046999958eb7e68
Bbatres@ <135394133+BATRUCHO@users.noreply.github.com>
2026-02-25 20:21:23 -0600

    cambios

--- Java/Proyectos/CasoEstudio/src/main/java/com/mycompany/casoestudio/Estudiante.java
+++ Java/Proyectos/CasoEstudio/src/main/java/com/mycompany/casoestudio/Estudiante.java
@@ -1,5 +1,34 @@
 package com.mycompany.casoestudio;
 
-public class Estudiante {
+import java.util.ArrayList;
+
+public class Estudiante extends Persona {
+    private String programaAcademico;
+    private  ArrayList<Calificacion> calificaciones;
+
+    protected  Estudiante(String nombre, String apellido, String id) {
+        super(nombre, apellido, id);
+        this.programaAcademico = programaAcademico;
+        this.calificaciones = new ArrayList<>();
+
+    }
+     //metodos getters
+
+    public String getProgramaAcademico() {
+        return programaAcademico;
+    }
+
+    //metodos //
+
+    public void obtenerPromedioGeneral() {
+        // Implementar lógica para calcular el promedio general del estudiante
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "programaAcademico=" + programaAcademico + ", calificaciones=" + calificaciones + '}';
+    }
+
     
+
 }
--- Java/Proyectos/CasoEstudio/src/main/java/com/mycompany/casoestudio/Persona.java
+++ Java/Proyectos/CasoEstudio/src/main/java/com/mycompany/casoestudio/Persona.java
@@ -1,1 +1,56 @@
 package com.mycompany.casoestudio;
+
+import java.time.LocalDate;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author brayan
+ */
+
+public abstract class Persona {
+
+    //Atributos
+    protected final String id;
+    protected LocalDate fechaNacimiento;
+    protected String nombre;
+    protected String apellido;
+    protected String correoElectronico;
+
+    //Constructor
+    public Persona(String id, LocalDate fechaNacimiento, String nombre, String apellido, String correoElectronico) {
+        this.id = id;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+    }
+
+    //Metodos getters 
+    public String getId() {
+        return id;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+ 
+
+}
+
